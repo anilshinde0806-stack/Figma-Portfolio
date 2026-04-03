21b79f513395bcbcdb6be38875b7d58e66915afa
Anil Shinde <anilshinde0806@gmail.com>
2026-04-03 20:26:01 +0530

    Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 export default function Header(): React.JSX.Element {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-[#110720]/80 backdrop-blur-sm border-b border-white/10">
-      <nav className="px-6 py-4">
+      <nav className="px-6 py-2">
         <div className="container mx-auto max-w-6xl flex items-center justify-between h-full">
           <Link 
             href="/" 
             className="text-2xl font-bold text-white hover:text-purple-400 transition-colors"
           >
-          <Image src="/logo/logo2.svg" alt="Logo" width={100} height={100} style={{ width: "auto", height: "auto" }} />
+          <Image src="/logo/logo2.svg" alt="Logo" width={100} height={10}  />
           </Link>
           <ul className="flex items-center gap-8 list-none m-0 p-0">
             <li className="m-0 p-0">
@@ -23,10 +23,18 @@ export default function Header(): React.JSX.Element {
             </li>
             <li className="m-0 p-0">
               <Link 
-                href="#about" 
+                href="#about"
                 className="text-white hover:text-purple-400 transition-colors text-base font-normal"
               >
                 About
+              </Link>
+            </li>
+            <li className="m-0 p-0">
+              <Link
+                href="#Experience"
+                className="text-white hover:text-purple-400 transition-colors text-base font-normal"
+              >
+               Work Experience
               </Link>
             </li>
             <li className="m-0 p-0">
@@ -34,7 +42,23 @@ export default function Header(): React.JSX.Element {
                 href="#lab" 
                 className="text-white hover:text-purple-400 transition-colors text-base font-normal"
               >
-                Lab
+               Certification
+              </Link>
+            </li>
+            <li className="m-0 p-0">
+              <Link
+                href="#Achievements"
+                className="text-white hover:text-purple-400 transition-colors text-base font-normal"
+              >
+               Achievements
+              </Link>
+            </li>
+            <li className="m-0 p-0">
+              <Link
+                href="#Footer"
+                className="text-white hover:text-purple-400 transition-colors text-base font-normal"
+              >
+               Contact
               </Link>
             </li>
           </ul>
