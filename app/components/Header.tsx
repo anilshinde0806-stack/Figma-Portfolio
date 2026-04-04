398b18f4fd4ffe39be74465d662441976c4f1b39
VANSH SHINDE <anilshinde0806@gmail.com>
2026-04-04 21:30:03 +0530

    04042026-2

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import {ReactNode, useState} from "react";
-import { motion } from "framer-motion";
+
 
 function Button(props: { size: string, onClick: () => void, children: ReactNode }) {
   return null;
